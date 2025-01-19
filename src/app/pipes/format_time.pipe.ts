731ce1c903924ef7dfbf7360d5853a9dac9d134d
Billy Yanez <bemonio@gmail.com>
2025-01-19 00:32:33 +0000

    style: remove comments

--- src/app/pipes/format_time.pipe.ts
+++ src/app/pipes/format_time.pipe.ts
@@ -9,7 +9,7 @@ export class FormatTimePipe implements PipeTransform {
 
     const [hours, minutes] = value.split(':').map(Number);
     const period = hours >= 12 ? 'PM' : 'AM';
-    const formattedHours = hours % 12 || 12; // Convierte 0 a 12
+    const formattedHours = hours % 12 || 12;
 
     return `${formattedHours}:${minutes.toString().padStart(2, '0')} ${period}`;
   }
